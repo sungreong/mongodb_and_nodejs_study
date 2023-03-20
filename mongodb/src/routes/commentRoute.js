4d814fedc41413fa72b01bc9cd9d91455bc9283a
lsr <leesungreong@gmail.com>
2023-03-20 20:15:19 +0900

    Transaction - 데이터 일관성 보장

--- mongodb/src/routes/commentRoute.js
+++ mongodb/src/routes/commentRoute.js
@@ -10,11 +10,14 @@ const { Comment } = require("./../models/Comment")
     - 특정 blog의 후기를 불러옴
     - 그러기 때문에 하위 개념
 */
-const { isValidObjectId } = require("mongoose");
+const { isValidObjectId, startSession } = require("mongoose");
 const { Blog } = require("./../models/Blog");
 const { User } = require("./../models/User");
 commentRouter.post("/", async (req, res) => {
+    const session = await startSession();
+    let comment
     try {
+        // await session.withTransaction(async () => {
         const { blogId } = req.params;
         const { content, userId } = req.body;
         if (!isValidObjectId(blogId)) return res.status(400).send({ err: "blogId is invalid" })
@@ -24,14 +27,16 @@ commentRouter.post("/", async (req, res) => {
         // const user = await User.findById(userId);
         // promise를 통해 비동기 처리하면 훨씬 효과적임
         const [blog, user] = await Promise.all([
-            Blog.findById(blogId),
-            User.findById(userId),
+            // find에다 concurrency를 방지하기 위해 session을 추가해야 함.
+            Blog.findById(blogId, {}, {}),
+            User.findById(userId, {}, {}),
         ])
         if (!blog || !user) return res.status(400).send({ err: "blog or user does not exist" })
         if (!blog.islive) return res.status(400).send({ err: "blog is not available" })
-        const comment = new Comment({ content, user, userFullName: `${user.name.first} ${user.name.last}`, blog: blogId }); // .limit(5)
+        comment = new Comment({ content, user, userFullName: `${user.name.first} ${user.name.last}`, blog: blogId }); // .limit(5)
 
-        // 개선
+        // await session.abortTransaction() // transaction 발생하는 데이터 수정은 다 원복이 됨.
+        // 개선(아래 코드로 개선)
         // await Promise.all([
         //     comment.save(),
         //     // DB에 일부 과부하를 주긴 함.
@@ -40,19 +45,35 @@ commentRouter.post("/", async (req, res) => {
         //     Blog.updateOne({ _id: blogId }, { $push: { comments: comment } })
         // ])
         // 자식 문서가 아니라 가공 문서를 내장하는 방식
-        blog.commentsCount++;
-        // blog안에 comments를 넣었는데 또 comment를 넣고 무한 루프 발생
-        blog.comments.push(comment);
-        if (blog.commentsCount > 3) blog.comments.shift();
-        // shift : a.shift 
+        // blog.commentsCount++;
+        // // blog안에 comments를 넣었는데 또 comment를 넣고 무한 루프 발생
+        // blog.comments.push(comment);
+        // if (blog.commentsCount > 3) blog.comments.shift();
+        // // shift : a.shift 
+        // await Promise.all([
+        //     comment.save({}),
+        //     // 아래는 필요 없음 이미 session을 통해서 불러오기 때문에
+        //     blog.save()
+        //     // Blog.updateOne({ _id: blogId }, { $inc: { commentsCount: 1 } })
+        // ])
+        // );
+        // 개선 버전 2 
+        // -3 : 제일 최근에 push된 3개만 살리고 다 뺴라 (우리가 원하는 구조)
+        // +3 : 제일 오래전에 push된 3개만 살리고 다 뺴라
         await Promise.all([
             comment.save(),
-            blog.save()
-            // Blog.updateOne({ _id: blogId }, { $inc: { commentsCount: 1 } })
-        ])
+            Blog.updateOne({ _id: blogId },
+                // 내부 transaction 사이에서는 atomicity를 따라서 transaction을 굳이 사용하지 않아도 된다는 장점.
+                // 내장을 잘하는 것이 중요함.
+                {
+                    $inc: { commentsCount: 1 },
+                    $push: { comments: { $each: [comment], $slice: -3 } }
+                }),])
         return res.send({ comment })
     } catch (err) {
         return res.status(400).send({ err: err.message })
+    } finally {
+        // await session.endSession();
     }
 
 })
